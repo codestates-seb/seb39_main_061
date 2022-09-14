17e08d6d4d9c0fd34645105c6f78892dc3244caa
Park Yun Taek <yountaek11@gmail.com>
2022-09-14 09:43:04 +0900

    Feat: Entity setting

--- server/src/main/java/com/project/QR/sector/entity/Sector.java
+++ server/src/main/java/com/project/QR/sector/entity/Sector.java
@@ -1,11 +1,19 @@
 package com.project.QR.sector.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Sector {
   @Id
   @GeneratedValue
