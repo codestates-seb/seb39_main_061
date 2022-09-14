3f3c97555870ddd0af5fce7036753cb44e85bdbc
Park Yun Taek <yountaek11@gmail.com>
2022-09-14 09:29:10 +0900

    Fix: Member entity role edit

--- server/src/main/java/com/project/QR/member/entity/Member.java
+++ server/src/main/java/com/project/QR/member/entity/Member.java
@@ -3,13 +3,18 @@ package com.project.QR.member.entity;
 import javax.persistence.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.QR.sector.entity.Sector;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+@Setter
 @Getter
 @Entity
 @NoArgsConstructor
@@ -26,33 +31,44 @@ public class Member {
   @JsonIgnore
   private String password;
 
-  @Enumerated(EnumType.STRING)
-  private Authority authority;
-
   @Column(nullable = false)
-  private String phone;
+  private String role;
 
+  @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private AuthProvider provider;
 
   @Column(nullable = false)
   private Boolean emailVerified = false;
 
-  @Enumerated(EnumType.STRING)
-  private Sector sector;
-
   private String profileImg;
 
+  private String phone;
+
+  private String businessName;
+
+  @ManyToOne
+  @JoinColumn(name = "SECTOR_ID")
+  private Sector sector;
+
+  public List<String> getRoleList() {
+    if(this.role.length() > 0) {
+      return Arrays.asList(this.role.split(","));
+    }
+    return new ArrayList<>();
+  }
   @Builder
-  public Member(String email, String name, String password, String phone, Authority authority,
-                AuthProvider provider, String profileImg) {
+  public Member(String email, String name, String password, String role, String businessName,
+                AuthProvider provider, String profileImg, Sector sector, String phone) {
     this.email = email;
     this.name = name;
-    this.phone = phone;
     this.password = password;
-    this.authority = authority;
+    this.role = role;
     this.provider = provider;
     this.profileImg = profileImg;
+    this.sector = sector;
+    this.phone = phone;
+    this.businessName = businessName;
   }
 }
 
