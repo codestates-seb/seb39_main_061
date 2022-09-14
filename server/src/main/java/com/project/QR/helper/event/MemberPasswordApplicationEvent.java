9ac6c8eeddce5f3fe8389b373439077b34481c04
Park Yun Taek <yountaek11@gmail.com>
2022-09-15 02:26:47 +0900

    Feat: reIssue Password event add

--- server/src/main/java/com/project/QR/helper/event/MemberPasswordApplicationEvent.java
+++ server/src/main/java/com/project/QR/helper/event/MemberPasswordApplicationEvent.java
@@ -1,2 +1,18 @@
-package com.project.QR.helper.event;public class MemberPasswordApplicationEvent {
+package com.project.QR.helper.event;
+
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class MemberPasswordApplicationEvent extends ApplicationEvent {
+  private String name;
+  private String password;
+  private String email;
+
+  public MemberPasswordApplicationEvent(Object source, String email, String name, String password) {
+    super(source);
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
 }
