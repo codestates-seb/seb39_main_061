128bf6b0507d606f61370712928f77498cee6d65
doityoo <yitsky@naver.com>
2022-09-14 09:44:17 +0900

    Feat: Dashboard/chartBar nivo 라이브러리 적용

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,24 @@
 import { Link } from "react-router-dom";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import ChartBar from "../../components/ChartBar/ChartBar";
 
 const Dashboard = () => {
+
   return (
     <div>
-      <h1>Dashboard Page</h1>
       <Sidebar />
+      <h1>Dashboard</h1>
+      <div>
+        <div>
+          <ChartBar />
+          <button>Download</button>
+        </div>
+        <div>Calendar</div>
+        <div>
+          <div>QR Code List</div>
+        </div>
+        <div>파이 그래프</div>
+      </div>
     </div>
   );
 };
