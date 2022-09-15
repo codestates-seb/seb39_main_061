9bfb31353beb6c2bc32fa2ace41b2182aec30241
dukjjang <jhd7292@naver.com>
2022-09-15 23:25:07 +0900

    feat: register 페이지 추가 기입 form 완성

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import { getLoginCookie } from "../../library/cookie";
@@ -6,6 +7,11 @@ import { authActions } from "../../store/auth";
 import { userAction } from "../../store/user";
 
 const Register = () => {
+  const BusinessCategoryRef = useRef();
+  const businessNameRef = useRef();
+  const phoneNumRef = useRef();
+  const nameRef = useRef();
+
   const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -19,6 +25,17 @@ const Register = () => {
 
   console.log(token);
   console.log("이메일 인증 안됌 ? = ", validationIsFalse);
+
+  const handlerSubmit = (e) => {
+    e.preventDefalut();
+    axios.patch("http://localhost:8080/auth/members", {
+      service: "reservation",
+      sectorId: BusinessCategoryRef.current.value,
+      businessName: businessNameRef.current.value,
+      phone: phoneNumRef.current.value,
+      name: nameRef.current.value,
+    });
+  };
 
   // 인증이 됐으면 로그인하기
   if (validationIsFalse === false) {
@@ -44,6 +61,31 @@ const Register = () => {
     <div>
       <h1>추가정보 등록 페이지</h1>
       <p>{location.search}</p>
+
+      <form onSubmit={handlerSubmit}>
+        <select ref={BusinessCategoryRef}>
+          <option value={0}>업종을 선택하세요</option>
+          <option value={1}>농업, 임업 및 어업</option>
+          <option value={2}>전기, 가스 및 수도사업</option>
+          <option value={3}>하수·폐기물 처리, 원료재생 및 환경복원업</option>
+          <option value={4}>건설업</option>
+          <option value={5}>숙박 및 음식점업</option>
+          <option value={6}>출판, 영상, 방송통신 및 정보서비스업</option>
+          <option value={7}>금융 및 보험업</option>
+          <option value={8}>부동산 및 임대업</option>
+          <option value={9}>전문, 과학 및 기술 서비스업</option>
+          <option value={10}>공공행정, 국방 및 사회보장 행정</option>
+          <option value={11}>교육 서비스업</option>
+          <option value={12}>보건 및 사회복지사업</option>
+          <option value={13}>예술, 스포츠 및 여가관련 서비스업</option>
+          <option value={14}>협회 및 단체, 수리 및 기타 개인서비스업</option>
+          <option value={15}>기타</option>
+        </select>
+        <input ref={businessNameRef} placeholder="상호명" />
+        <input ref={phoneNumRef} placeholder="전화번호" />
+        <input ref={nameRef} placeholder="이름" />
+        <button>전송</button>
+      </form>
     </div>
   );
 };
