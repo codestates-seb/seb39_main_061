75e2bfc4b651a3556987e384bbcea53b56e5d78f
Park Yun Taek <yountaek11@gmail.com>
2022-09-18 23:13:30 +0900

    Fix: MemberDetails memberId edit

--- server/src/main/java/com/project/QR/security/MemberDetailsService.java
+++ server/src/main/java/com/project/QR/security/MemberDetailsService.java
@@ -3,12 +3,15 @@ package com.project.QR.security;
 import com.project.QR.member.entity.Member;
 import com.project.QR.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Service
 @Transactional
@@ -17,7 +20,7 @@ public class MemberDetailsService implements UserDetailsService {
   private final MemberRepository memberRepository;
 
   @Override
-  public MemberDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
     Member member = memberRepository.findByEmail(email).orElse(null);
     return MemberDetails.create(member);
   }
