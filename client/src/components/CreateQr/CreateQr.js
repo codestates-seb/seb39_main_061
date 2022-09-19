d553d53aa101d10cf7de4ae40d35fc399009cc74
Park Yun Taek <yountaek11@gmail.com>
2022-09-19 10:45:24 +0900

    Feat: CreateQr update

--- client/src/components/CreateQr/CreateQr.js
+++ client/src/components/CreateQr/CreateQr.js
@@ -2,12 +2,13 @@ import QRCode from 'qrcode'
 import { useState } from 'react'
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import axios from 'axios';
 
 function CreateQr() {
 	const [url, setUrl] = useState('')
 	const [qr, setQr] = useState('')
 	const [formData, setFormData] = useState(new FormData)
-	const [data, setData] = useState({ target: '', businessName: "test", dueDate: new Date() });
+	const [body, setBody] = useState({ target: '', businessName: "test", dueDate: new Date(), qrType: "reservation" });
 
 	const GenerateQRCode = () => {
 		
@@ -22,8 +23,7 @@ function CreateQr() {
 			if (err) return console.error(err)
 			
 			setQr(url)
-			const form = new FormData;
-			form.append("file", dataURLtoBlob(url), "qr.png");
+			
 		})
 	}
 
@@ -42,47 +42,70 @@ function CreateQr() {
 	} 
 
 	const saveQRCode = async () => {
-		const formData = new FormData;
-		formData.append("data", new Blob([JSON.stringify(data)], { type: "application/json" }))
-		/**
-		 * response를 받아와서 정상이라면 update api 호출
+		
+		const {data: {data, message}, status} = await axios.post("http://localhost:8080/api/v1/qr-code/reservation", 
+			body,
+			{
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem("token")}`,
+				}
+			}
+		);
+		console.log(status)
+		console.log(message)
+		if(message === "CREATED") {
+			/**
+			 * response를 받아와서 정상이라면 update api 호출
 		 */
-		// const response = await axios.post("http://localhost:8080/api/v1/qr-code", 
-		// 	formData,
-		// 	{
-		// 		headers: {
-		// 			Authorization: "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5aXRza3lAbmF2ZXIuY29tIiwicm9sZSI6IlJPTEVfUkVTRVJWQVRJT04iLCJpYXQiOjE2NjMyMjkwOTQsImV4cCI6MTY2MzIzMjY5NH0.4NqshdzvtnPM7qqh0PwsjsHuHVHCoN3a0mIb_JHtJOplIX2cqr3IHDyLk2ihp3hS2V5ZBgVnRHhDuLkHwYl_0w",
-		// 			"Content-Type": "multipart/form-data"
-		// 		}
-		// 	}
-		// );
-
-
-
-		/**
-		 * response 데이터에 qr 코드 아이디가 날라올텐데 이를 이용하여 qr 코드 이미지 생성 후 update api 호출
-		 * GnerateQRCode function 이용
-		 */
-
-
-
-		/**
-		 * 이후 QR 코드 상세 페이지로 이동
-		 */
+			// TO-DO: 추후 URL 변경 필요, 세부적인 에러 처리 필요!
+			console.log(data)
+			console.log("저장 성공 " + url)
+			QRCode.toDataURL(`${window.location.origin}/reservation/${data.qrCodeId}?target=${data.target}`, {
+				width: 320,
+				 height: 320,
+				color: {
+					dark: '#000000',
+					light: '#EEEEEEFF'
+				}
+			}, (err, url) => {
+				if (err) return console.error(err)
+				
+				setQr(url)
+				const formData = new FormData;
+				formData.append("data", new Blob([JSON.stringify(body)], { type: "application/json" }))
+				formData.append("file", dataURLtoBlob(url), "qr.png")
+				console.log(dataURLtoBlob(url))
+				const response = axios.post(`http://localhost:8080/api/v1/qr-code/reservation/${data.qrCodeId}/update`, 
+					formData,
+					{
+						headers: {
+							Authorization: `Bearer ${localStorage.getItem("token")}`,
+							"Content-Type": "multipart/form-data"
+						}
+					}
+				).then((response) => {
+					/**
+					 * 이후 QR 코드 상세 페이지로 이동
+					 */
+					console.log(response);
+				});
+			})
+		}
+		
 	}
 
 	return (
 		<div className="app">
 			<h1>QR 코드 생성</h1>
-			{/* <img src={qr} /> */}
+			<img src={qr} />
 			<div>관리할 대상입니다.</div>
 			<input 
 				type="text"
 				placeholder="대상명을 적어주세요"
-				value={data.target}
-				onChange={e => setData((prevState) => {return {...prevState, target: e.target.value }})} />
+				value={body.target}
+				onChange={e => setBody((prevState) => {return {...prevState, target: e.target.value }})} />
 			<div>만료 기간을 선택해주세요.</div>				
-			<Calendar onChange={e => setData((prevState) => {return {...prevState, dueDate: e }})} value={data.dueDate} />
+			<Calendar onChange={e => setBody((prevState) => {return {...prevState, dueDate: new Date(e - new Date().getTimezoneOffset() * 60000) }})} value={body.dueDate} />
 			<button onClick={saveQRCode}>생 성</button>
 			<button onClick={CancelQRCode}>취 소</button>
 		</div>
