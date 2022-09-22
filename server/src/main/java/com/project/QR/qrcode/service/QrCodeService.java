fba943431b813c956b94873cf4a1732e194f160d
Park Yun Taek <yountaek11@gmail.com>
2022-09-22 09:37:47 +0900

    Feat: QR코드 생성시 확인 로직 추가

--- server/src/main/java/com/project/QR/qrcode/service/QrCodeService.java
+++ server/src/main/java/com/project/QR/qrcode/service/QrCodeService.java
@@ -32,6 +32,7 @@ public class QrCodeService {
    * QrCode 등록
    */
   public QrCode createQrCode(QrCode qrCode) {
+    businessService.getBusiness(qrCode.getBusiness().getBusinessId(), qrCode.getBusiness().getMember().getMemberId());
     return qrCodeRepository.save(qrCode);
   }
 
