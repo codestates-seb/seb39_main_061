eb08b29fd12c7bf1438f53d4ec6dfa5c69f1325b
Park Yun Taek <parkyountaek@users.noreply.github.com>
2022-09-23 19:57:49 +0900

    Feat/restdocs6 (#62)
    * feat: 새로고침 로그아웃 추가
    
    * feat: axios 리팩토링
    
    * fix: 비밀번호 체크 메시지 수정
    
    * Chore: env update
    
    * fix: axios 리팩토링 및 ouath 수정
    
    * Feat: package.json 추가
    
    * Chore: env update
    
    * Chore: env update
    
    * Chore: env update
    
    * Chore: env update
    
    - OAuth2 중복 가입 허용
    
    * Feat: 예약관련 RestDocs 작성
    
    - 예약(사용자) docs 작성
    - 통계 docs 작성
    - QR 코드 docs 작성
    
    * Chore: env update
    
    restdocs add
    
    * 소셜 회원가입 테스트 (#50)
    
    * design: 로그인 페이지 css 디자인, 로그인 유효성검사 구현
    
    * feat: register,signup 페이지 css, 유효성검사 추가
    
    * design: register 페이지 css 수정수
    
    * design: signup 반응형 css 수정
    
    * design: register 페이지  반응형 css 수정
    
    * design: register css 수정
    
    * feat: 회원가입 유효성 검사 추가 및 css 수정
    
    * design: 메시지  모달 컴포넌트 생성, css 디자인 완성
    
    * Chore: env update
    
    OAuth2 추가 정보 기입 시 Business에 MemberId 안들어가는 문제 해결
    
    * Fix: URI 수정
    
    * Fix: 삭제시 이미지 파일 존재 여부에 따른 파일 삭제
    
    * Test: Menu RestDocs 작성
    
    * Feat: env update
    
    * Test: Review Restdocs 구조 세팅
    
    * Test: 업주 입장 예약관련 restdocs 변경
    
    * Test: snippets 이름 수정
    
    * Test: RestDocs 업데이트
    
    * Chore: env update
    
    * Fix: 함수 이름 변경(업주/사용자 분기)
    
    * Test: Review restdocs 작성
    
    * Revmoe: Docker 삭제
    
    Co-authored-by: dukjjang <jhd7292@naver.com>
    Co-authored-by: doityoo <82711000+doityoo@users.noreply.github.com>
    Co-authored-by: doityoo <yitsky@naver.com>
    Co-authored-by: Jinji <102455275+dukjjang@users.noreply.github.com>

--- client/src/pages/EmailValidation/EmailValidation.js
+++ client/src/pages/EmailValidation/EmailValidation.js
@@ -2,46 +2,50 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import baseURL from "../../library/axios";
+import { baseURL } from "../../library/axios";
 import styles from "./EmailValidation.module.css";
 
 const EmailValidation = () => {
   const [validation, setValidation] = useState(true);
   const navigate = useNavigate();
   const location = useLocation();
+  console.log(location.search);
+  let email = String(location.search.split("&", 1));
+  email = email.slice(7);
+
   useEffect(() => {
-    axios
-      .get(`http://localhost:8080/auth/validation${location.search}`)
-      .then((res) => {
-        setValidation(true);
-        alert("인증 성공!");
-        setTimeout(() => {
-          navigate("/login");
-        }, 5000);
-      })
+    const res = axios
+      .get(`${baseURL}/auth/validation${location.search}`)
       .catch((err) => {
         setValidation(false);
         if (err.response) {
           const errMsg = err.response.data;
           console.log(errMsg);
-
-          //   setTimeout(() => {
-          //     navigate("/signup");
-          //   }, 5000);
         }
       });
+    if (res) {
+      setValidation(true);
+      alert("인증 성공!");
+      setTimeout(() => {
+        navigate("/login");
+      }, 3000);
+    }
   }, []);
 
   return (
     <section className={styles.EmailValidation}>
-      {validation ? (
-        <h1>이메일 인증이 성공하였습니다!</h1>
-      ) : (
-        <h1>이메일 인증이 실패하였습니다!</h1>
-      )}
-      {validation && <p>잠시후 로그인 페이지로 이동합니다</p>}
-      {!validation && <p>잠시후 회원가입 페이지로 이동합니다</p>}
-      {!validation && <button>이메일 다시 보내기</button>}
+      <div>
+        {validation ? (
+          <h1>
+            {email}님의 회원가입을 축하합니다. 로그인 후 QuickBook의 모든
+            서비스를 이용해보세요.
+          </h1>
+        ) : (
+          <h1>이메일 인증이 실패하였습니다</h1>
+        )}
+        {validation && <p>잠시후 로그인 페이지로 이동합니다</p>}
+        {!validation && <p>다시 시도해주세요 회원가입 페이지로 이동합니다 </p>}
+      </div>
     </section>
   );
 };
