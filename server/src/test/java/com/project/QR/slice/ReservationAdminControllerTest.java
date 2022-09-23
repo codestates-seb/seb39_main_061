7f0e49da79c804f44497e5c599a6b8804959d659
Park Yun Taek <parkyountaek@users.noreply.github.com>
2022-09-23 17:02:12 +0900

    Feat/restdocs6 (#61)
    * Feat: 예약관련 RestDocs 작성
    
    - 예약(사용자) docs 작성
    - 통계 docs 작성
    - QR 코드 docs 작성
    
    * Fix: URI 수정
    
    * Fix: 삭제시 이미지 파일 존재 여부에 따른 파일 삭제
    
    * Test: Menu RestDocs 작성
    
    * Test: Review Restdocs 구조 세팅
    
    * Test: 업주 입장 예약관련 restdocs 변경
    
    * Test: snippets 이름 수정
    
    * Test: RestDocs 업데이트

--- server/src/test/java/com/project/QR/slice/ReservationAdminControllerTest.java
+++ server/src/test/java/com/project/QR/slice/ReservationAdminControllerTest.java
@@ -1,11 +1,16 @@
 package com.project.QR.slice;
 
 import com.google.gson.Gson;
+import com.project.QR.dto.MessageResponseDto;
+import com.project.QR.dto.MultiResponseWithPageInfoDto;
 import com.project.QR.helper.WithMockCustomUser;
 import com.project.QR.reservation.controller.ReservationAdminController;
 import com.project.QR.reservation.dto.ReservationResponseDto;
+import com.project.QR.reservation.entity.Reservation;
 import com.project.QR.reservation.mapper.ReservationMapper;
 import com.project.QR.reservation.service.ReservationService;
+import com.project.QR.security.MemberDetails;
+import com.project.QR.stub.ReservationStubData;
 import com.project.QR.stub.StatisticsStubData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,22 +20,33 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.validation.constraints.Positive;
+import javax.websocket.server.PathParam;
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.project.QR.util.ApiDocumentUtils.getRequestPreProcessor;
 import static com.project.QR.util.ApiDocumentUtils.getResponsePreProcessor;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
@@ -69,7 +85,7 @@ public class ReservationAdminControllerTest {
 
     // when
     ResultActions actions = mockMvc.perform(
-      get("/api/v1/reservation/{business-id}/qr-code/{qr-code-id}/statistics?date={date}", businessId, qrCodeId, date)
+      get("/api/v1/business/{business-id}/reservation/qr-code/{qr-code-id}/statistics?date={date}", businessId, qrCodeId, date)
         .accept(MediaType.APPLICATION_JSON)
         .contentType(MediaType.APPLICATION_JSON)
         .header("Authorization", "Bearer {ACCESS_TOKEN}")
@@ -107,6 +123,154 @@ public class ReservationAdminControllerTest {
               fieldWithPath("data.time[].deleted").type(JsonFieldType.STRING).description("예약 취소 여부"),
               fieldWithPath("data.time[].date").type(JsonFieldType.STRING).description("시간대"),
               fieldWithPath("data.time[].count").type(JsonFieldType.NUMBER).description("예약 총 합"),
+              fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메시지")
+            )
+          )
+        )
+      );
+  }
+
+  @Test
+  @DisplayName("예약 리스트 조회 테스트")
+  public void getReservationListTest() throws Exception {
+    // given
+    long businessId = 1L, qrCodeId = 1L;
+    int page = 1;
+    int size = 10;
+
+    Page<Reservation> pageOfReservation = ReservationStubData.getReservationPage(page - 1, size);
+    List<Reservation> reservationList = pageOfReservation.getContent();
+    List<ReservationResponseDto.ReservationInfoDto> reservationInfoDtoList
+      = ReservationStubData.reservationInfoDtoList(reservationList);
+
+    given(reservationService.getAdminReservationList(Mockito.anyLong(), Mockito.anyLong(), Mockito.anyLong(),
+      Mockito.anyInt(), Mockito.anyInt())).willReturn(pageOfReservation);
+    given(mapper.reservationListToReservationInfoDtoList(Mockito.anyList())).willReturn(reservationInfoDtoList);
+
+    // when
+    ResultActions actions = mockMvc.perform(
+      get("/api/v1/business/{business-id}/reservation/qr-code/{qr-code-id}?page={page}&size={size}",
+        businessId, qrCodeId, page, size)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("Authorization", "Bearer {ACCESS_TOKEN}")
+    );
+
+    // then
+    actions
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.pageInfo.page").value(page))
+      .andExpect(jsonPath("$.pageInfo.size").value(size))
+      .andExpect(jsonPath("$.pageInfo.totalElements").value(pageOfReservation.getTotalElements()))
+      .andDo(
+        document(
+          "reservation-list-get-admin",
+          getRequestPreProcessor(),
+          getResponsePreProcessor(),
+          requestHeaders(headerWithName("Authorization").description("Bearer AccessToken")),
+          pathParameters(
+            parameterWithName("business-id").description("사업 식별자"),
+            parameterWithName("qr-code-id").description("QR 코드 식별자")
+          ),
+          requestParameters(
+            parameterWithName("page").description("페이지 수"),
+            parameterWithName("size").description("페이지 크기")
+          ),
+          responseFields(
+            List.of(
+              fieldWithPath("data").type(JsonFieldType.ARRAY).description("결과 데이터"),
+              fieldWithPath("data[].reservationId").type(JsonFieldType.NUMBER).description("예약 식별자"),
+              fieldWithPath("data[].name").type(JsonFieldType.STRING).description("예약자 이름"),
+              fieldWithPath("data[].phone").type(JsonFieldType.STRING).description("예약자 연락처"),
+              fieldWithPath("data[].createdAt").type(JsonFieldType.STRING).description("예약 등록일"),
+              fieldWithPath("data[].completed").type(JsonFieldType.STRING).description("예약 입장 완료 여부"),
+              fieldWithPath("data[].count").type(JsonFieldType.NUMBER).description("예약자 수"),
+              fieldWithPath("pageInfo").type(JsonFieldType.OBJECT).description("페이지 정보"),
+              fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("현재 페이지"),
+              fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("현재 페이지 크기"),
+              fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("총 데이터 수"),
+              fieldWithPath("pageInfo.totalPages").type(JsonFieldType.NUMBER).description("총 페이지 수")
+            )
+          )
+        )
+      );
+  }
+
+  @Test
+  @DisplayName("예약 입장 테스트")
+  public void enterReservationTest() throws Exception {
+    // given
+    long businessId = 1L, qrCodeId = 1L, reservationId = 1L;
+
+    doNothing().when(reservationService).enterReservation(Mockito.anyLong(), Mockito.anyLong(), Mockito.anyLong(), Mockito.anyLong());
+
+    // when
+    ResultActions actions = mockMvc.perform(
+      patch("/api/v1/business/{business-id}/reservation/qr-code/{qr-code-id}/info/{reservation-id}/enter",
+        businessId, qrCodeId, reservationId)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("Authorization", "Bearer {ACCESS_TOKEN}")
+    );
+
+    // then
+    actions
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.message").value("SUCCESS"))
+      .andDo(
+        document(
+          "reservation-enter",
+          getRequestPreProcessor(),
+          getResponsePreProcessor(),
+          requestHeaders(headerWithName("Authorization").description("Bearer AccessToken")),
+          pathParameters(
+            parameterWithName("business-id").description("사업 식별자"),
+            parameterWithName("qr-code-id").description("QR 코드 식별자"),
+            parameterWithName("reservation-id").description("예약 식별자")
+          ),
+          responseFields(
+            List.of(
+              fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메시지")
+            )
+          )
+        )
+      );
+  }
+
+  @Test
+  @DisplayName("예약 취소 테스트")
+  public void cancelReservationTest() throws Exception{
+    // given
+    long businessId = 1L, qrCodeId = 1L, reservationId = 1L;
+
+    doNothing().when(reservationService).enterReservation(Mockito.anyLong(), Mockito.anyLong(), Mockito.anyLong(), Mockito.anyLong());
+
+    // when
+    ResultActions actions = mockMvc.perform(
+      patch("/api/v1/business/{business-id}/reservation/qr-code/{qr-code-id}/info/{reservation-id}/cancel",
+        businessId, qrCodeId, reservationId)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("Authorization", "Bearer {ACCESS_TOKEN}")
+    );
+
+    // then
+    actions
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.message").value("SUCCESS"))
+      .andDo(
+        document(
+          "reservation-cancel",
+          getRequestPreProcessor(),
+          getResponsePreProcessor(),
+          requestHeaders(headerWithName("Authorization").description("Bearer AccessToken")),
+          pathParameters(
+            parameterWithName("business-id").description("사업 식별자"),
+            parameterWithName("qr-code-id").description("QR 코드 식별자"),
+            parameterWithName("reservation-id").description("예약 식별자")
+          ),
+          responseFields(
+            List.of(
               fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메시지")
             )
           )
