f5f94ba133717953d3715e9bf5d9658b46508e13
ForestRimm <yourim0914@gmail.com>
2022-09-26 16:31:41 +0900

    Feat: QrCodeKeepController 추가

---
+++ server/src/main/java/com/project/QR/qrcode/controller/QrCodeKeepController.java
@@ -0,0 +1,109 @@
+package com.project.QR.qrcode.controller;
+
+import com.project.QR.dto.MultiResponseWithPageInfoDto;
+import com.project.QR.dto.SingleResponseWithMessageDto;
+import com.project.QR.qrcode.dto.QrCodeRequestDto;
+import com.project.QR.qrcode.entity.QrCode;
+import com.project.QR.qrcode.mapper.QrCodeMapper;
+import com.project.QR.qrcode.service.QrCodeService;
+import com.project.QR.security.MemberDetails;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@Validated
+@RestController
+@RequestMapping("/api/v1/business/{business-id}/type/keep/qr-code")
+@AllArgsConstructor
+public class QrCodeKeepController {
+  private final QrCodeService qrCodeService;
+  private final QrCodeMapper mapper;
+
+  /**
+   * QrCode 생성 api
+   */
+  @PostMapping
+  public ResponseEntity createQrCode(@AuthenticationPrincipal MemberDetails memberDetails,
+                                     @Positive @PathVariable("business-id") long businessId,
+                                     @Valid @RequestBody QrCodeRequestDto.CreateQrCodeDto createQrCodeDto) {
+    createQrCodeDto.setBusinessId(businessId);
+    createQrCodeDto.setMemberId(memberDetails.getMember().getMemberId());
+    QrCode qrCode = qrCodeService.createQrCode(mapper.createQrCodeDtoToQrCode(createQrCodeDto));
+
+    return new ResponseEntity<>(new SingleResponseWithMessageDto<>(mapper.qrCodeToQrCodeInfoDto(qrCode),
+            "CREATED"),
+            HttpStatus.CREATED);
+  }
+
+  /**
+   * 특정 QrCode 조회 api
+   */
+  @GetMapping("/{qr-code-id}")
+  public ResponseEntity getQrCode(@AuthenticationPrincipal MemberDetails memberDetails,
+                                  @Positive @PathVariable("business-id") long businessId,
+                                  @Positive @PathVariable("qr-code-id")  long qrCodeId){
+    QrCode qrCode = qrCodeService.getQrCode(qrCodeId, businessId, memberDetails.getMember().getMemberId());
+
+    return new ResponseEntity<>(
+            new SingleResponseWithMessageDto<>(mapper.qrCodeToQrCodeInfoDto(qrCode),"SUCCESS"),
+            HttpStatus.OK);
+  }
+
+  /**
+   * 전체 QrCode 리스트 조회 api
+   */
+  @GetMapping
+  public ResponseEntity getKeepQrCodes(@AuthenticationPrincipal MemberDetails memberDetails,
+                                       @Positive @PathVariable("business-id") long businessId,
+                                       @Positive @PathParam("page") int page,
+                                       @Positive @PathParam("size") int size){
+    Page<QrCode> pageOfQrCode = qrCodeService.getQrCodes( page - 1, size, businessId, memberDetails.getMember().getMemberId());
+    List<QrCode> qrCodeList = pageOfQrCode.getContent();
+    return new ResponseEntity<>(new MultiResponseWithPageInfoDto<>(mapper.qrCodeListToQrCodeInfoDtoList(qrCodeList),
+            pageOfQrCode),
+            HttpStatus.OK);
+  }
+
+  /**
+   * QrCode 변경 api
+   */
+  @PostMapping(value = "/{qr-code-id}/update", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+  public ResponseEntity updateQrCode(@AuthenticationPrincipal MemberDetails memberDetails,
+                                     @Positive @PathVariable("business-id") long businessId,
+                                     @Positive @PathVariable("qr-code-id") long qrCodeId,
+                                     @Valid @RequestPart(name = "data") QrCodeRequestDto.UpdateQrCodeDto updateQrCodeDto,
+                                     @RequestPart(name = "file", required = false) MultipartFile multipartFile) {
+    updateQrCodeDto.setBusinessId(businessId);
+    updateQrCodeDto.setMemberId(memberDetails.getMember().getMemberId());
+    updateQrCodeDto.setQrCodeId(qrCodeId);
+    QrCode qrCode = qrCodeService.updateQrCode(mapper.updateQrCodeDtoToQrCode(updateQrCodeDto), multipartFile);
+
+    return new ResponseEntity<>(new SingleResponseWithMessageDto<>(mapper.qrCodeToQrCodeInfoDto(qrCode),
+            "SUCCESS"),
+            HttpStatus.OK);
+  }
+
+  /**
+   * QrCode 삭제 api
+   */
+  @DeleteMapping("/{qr-code-id}")
+  public ResponseEntity deleteQrCode(@AuthenticationPrincipal MemberDetails memberDetails,
+                                     @Positive @PathVariable("business-id") long businessId,
+                                     @Positive @PathVariable("qr-code-id") long qrCodeId) {
+
+    qrCodeService.deleteQrCode(qrCodeId, businessId, memberDetails.getMember().getMemberId());
+
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+}
