de00da836013b7da566fb49ffe4e451dbdca6ce1
Park Yun Taek <yountaek11@gmail.com>
2022-09-26 16:02:07 +0900

    Refacotr: Qr코드 api 조회시 reservation 정보 제거

--- server/src/main/java/com/project/QR/qrcode/dto/QrCodeResponseDto.java
+++ server/src/main/java/com/project/QR/qrcode/dto/QrCodeResponseDto.java
@@ -19,7 +19,6 @@ public class QrCodeResponseDto {
     private String qrCodeImg;
     private String target;
     private QrType qrType;
-    private List<ReservationResponseDto.ReservationInfoDto> reservations;
   }
   @Data
   @NoArgsConstructor
