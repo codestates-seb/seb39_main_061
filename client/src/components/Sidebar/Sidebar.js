a4875413ba633d3c2edfa333cc29ea7405264a2a
doityoo <yitsky@naver.com>
2022-09-27 13:51:21 +0900

    Feat:img 파일 변경, sidebar 에러 수정

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,38 +1,31 @@
 import { Link } from "react-router-dom";
 import styles from "./Sidebar.module.css";
 import logo from "../../Img/Asset_3.png";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../../store/auth";
+import { useNavigate } from "react-router-dom";
+import { persistor } from "../../index";
 
 const Sidebar = () => {
-  // const isLogin = useSelector((state) => state.auth.isAuthenticated);
-  // const dispatch = useDispatch();
-  // const navigate = useNavigate();
-  // const logoutHanlder = () => {
-  //   // deleteToken("token");
-  //   localStorage.removeItem("token");
-  //   dispatch(authActions.logout());
-  //   console.log("로그아웃");
-  //   navigate("/");
-  // };
-  // const purge = async () => {
-  //   await persistor.purge();
-  // };
-
-//   return (
-//     <div className={styles.sidebar_container}>
-//       <Link to="/dashboard">
-//         <img src={logo} alt="React" className={styles.logo}></img>
-// import { useSelector, useDispatch } from "react-redux";
-// import { authActions } from "../../store/auth";
-// import { useNavigate } from "react-router-dom";
-// import { persistor } from "../../index";
+  const isLogin = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logoutHanlder = () => {
+    // deleteToken("token");
+    localStorage.removeItem("token");
+    dispatch(authActions.logout());
+    console.log("로그아웃");
+    navigate("/login");
+  };
+  const purge = async () => {
+    await persistor.purge();
+  };
 
   return (
-    <div>
-      <Link to="/dashboard">
-        <button>dashboard</button>
-      </Link>
+    <div className={styles.sidebar_container}>
+      <img src={logo} className={styles.logo} />
       <Link to="/profile">
-        <img src="" className={styles.profile}></img>
+        <img src="" className={styles.profile} />
       </Link>
       <div className={styles.btnContainer}>
         <Link to="/dashboard">
@@ -53,17 +46,18 @@ const Sidebar = () => {
         <Link to="/">
           <button className={styles.componentsBtn}>리뷰 관리</button>
         </Link>
-      </div>
-      {/* {isLogin && (
+      {isLogin && (
         <button
-          onClick={async () => {
-            await logoutHanlder();
-            await setTimeout(() => purge(), 200);
-          }}
+        className={styles.componentsBtn}
+        onClick={async () => {
+          await logoutHanlder();
+          await setTimeout(() => purge(), 200);
+        }}
         >
           로그아웃
         </button>
-      )} */}
+      )}
+      </div>
     </div>
   );
 };
