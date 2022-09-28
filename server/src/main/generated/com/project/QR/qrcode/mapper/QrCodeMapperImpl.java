1fe2a6d6fa117bc85cc98fbb8064b8acaf360bc0
ForestRimm <yourim0914@gmail.com>
2022-09-28 16:59:03 +0900

    Refactor: 업데이트 반영

---
+++ server/src/main/generated/com/project/QR/qrcode/mapper/QrCodeMapperImpl.java
@@ -0,0 +1,33 @@
+package com.project.QR.qrcode.mapper;
+
+import com.project.QR.qrcode.dto.QrCodeResponseDto;
+import com.project.QR.qrcode.entity.QrCode;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-09-27T18:18:32+0900",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 11.0.16.1 (Azul Systems, Inc.)"
+)
+@Component
+public class QrCodeMapperImpl implements QrCodeMapper {
+
+    @Override
+    public QrCodeResponseDto.ShortQrCodeInfoDto qrCodeToShortQrCodeInfoDto(QrCode qrCode) {
+        if ( qrCode == null ) {
+            return null;
+        }
+
+        QrCodeResponseDto.ShortQrCodeInfoDto.ShortQrCodeInfoDtoBuilder shortQrCodeInfoDto = QrCodeResponseDto.ShortQrCodeInfoDto.builder();
+
+        if ( qrCode.getQrCodeId() != null ) {
+            shortQrCodeInfoDto.qrCodeId( qrCode.getQrCodeId() );
+        }
+        shortQrCodeInfoDto.qrCodeImg( qrCode.getQrCodeImg() );
+        shortQrCodeInfoDto.target( qrCode.getTarget() );
+        shortQrCodeInfoDto.qrType( qrCode.getQrType() );
+
+        return shortQrCodeInfoDto.build();
+    }
+}
