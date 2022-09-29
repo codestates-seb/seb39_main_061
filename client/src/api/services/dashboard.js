7d9cc7f05d34ed77fe2dae154a7e4ce8337dacef
doityoo <yitsky@naver.com>
2022-09-29 23:01:22 +0900

    Feat:#10 대시보드 서버 요청 테스트

---
+++ client/src/api/services/dashboard.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { getAuthorizationHeader } from "../axios";
+import { baseURL } from "../axios";
+
+export const getDashboard = (today) => {
+  return axios
+    .get(`${baseURL}http://localhost:8080/api/v1/business/1/reservation/qr-code/1/statistics?date=${today}`, {
+      headers: {
+        Authorization: getAuthorizationHeader(),
+      },
+    })
+    .then((res) => {
+      return res.data.data;
+    })
+    .catch((err) => {
+      console.log(err.response);
+    });
+};
