3a87aaef6759ee7f18a080919f140b520f254b75
dukjjang <jhd7292@naver.com>
2022-10-12 13:58:13 +0900

    fix: 예약자 업주 api통신 로직 수정

--- client/src/api/services/reservation-admin.js
+++ client/src/api/services/reservation-admin.js
@@ -1,8 +1,15 @@
+import axios from "axios";
 import { adminAxiosInstance } from "./instance";
+import { baseURL, getAuthorizationHeader } from "../axios";
 
 export const getAdminResList = (businessId, qrId) => {
-  return adminAxiosInstance.get(
-    `/business/${businessId}/reservation/qr-code/${qrId}?page=1&size=10`
+  return axios.get(
+    `${baseURL}/business/${businessId}/reservation/qr-code/${qrId}?page=1&size=10`,
+    {
+      headers: {
+        Authorization: getAuthorizationHeader(),
+      },
+    }
   );
 };
 
